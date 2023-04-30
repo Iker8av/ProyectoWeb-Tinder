9556b1ba6fcc2704fa7801509b781c19d9e99893
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-04-30 17:09:05 -0600

    Profile has editable inputs

--- frontend/src/components/Profile/Profile.jsx
+++ frontend/src/components/Profile/Profile.jsx
@@ -22,24 +22,24 @@ export default function Profile() {
                     <div className='profileInfo-header'>
                         <div>
                             <h3>Name:</h3>
-                            {edit ? <input type="text" id="name" name="name"/> : <h2>{name} </h2> }
+                            {edit ? <input value={name} onChange={(e) => setName(e.target.value)} type="text" id="name" name="name"/> : <h2>{name} </h2> }
                         </div>
                         <div>
                             <h3>Age:</h3>
-                            {edit ? <input type="number" id="age" name="age"/> : <h2>{age} </h2> }
+                            {edit ? <input value={age} onChange={(e) => setAge(e.target.value)} type="number" id="age" name="age"/> : <h2>{age} </h2> }
                         </div>
                     </div>
                     <div className='profile-row'>
                         <h3>Company:</h3>
-                        {edit ? <input type="text" id="company" name="company"/> : <h2>{company} </h2> }
+                        {edit ? <input value={company} onChange={(e) => setCompany(e.target.value)} type="text" id="company" name="company"/> : <h2>{company} </h2> }
                     </div>
                     <div className='profile-row'>
                         <h3>Looking For:</h3>
-                        {edit ? <input type="text" id="lookingFor" name="lookingFor"/> : <h2>{lookingFor} </h2> }
+                        {edit ? <input value={lookingFor} onChange={(e) => setLookingFor(e.target.value)} type="text" id="lookingFor" name="lookingFor"/> : <h2>{lookingFor} </h2> }
                     </div>
                     <div>
                         <h3>Message:</h3>
-                        {edit ? <textarea name="message" className='textarea'/> : <h2>{message} </h2> }
+                        {edit ? <textarea value={message} name="message" className='textarea'/> : <h2>{message} </h2> }
                     </div>
                     {/* <div>
                         <h3>Custom Audio:</h3>
