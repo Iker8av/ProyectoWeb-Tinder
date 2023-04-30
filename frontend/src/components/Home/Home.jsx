f98831427bfb339a481586827560bbccc1d62762
Iker Ochoa <79234651+Iker8av@users.noreply.github.com>
2023-04-30 14:34:21 -0600

    Fixed UI & hierarchy

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import UserCard from '../UserCard/UserCard'
+import UserCard from "../UserCard/UserCard"
+import axios from "axios";
 import "./Home.css"
 
 export default function Home() {
-    const [data, setData] = React.useState([1,2,3,4,5,6,7,8,9,11,13,20])
+    const [data, setData] = React.useState([])
     const [like, setLike] = React.useState(0) // 0.- No Action | 1.- Like | 2.- Dislike //
     const [transitionPerforming, setTransitionPerforming] = React.useState(false)
     const [playingAudio, setPlayingAudio] = React.useState(false)
@@ -25,11 +26,24 @@ export default function Home() {
         setTransitionPerforming(false)
     }
 
+
+    const loadData = async () => {
+        await axios.get("https://minireto-api.vercel.app/getUsers/").then((res) => {
+
+            setData([...res.data.data]);
+            console.log(res.data.data)
+        });
+    };
+
+    React.useEffect(() => {
+        loadData();
+    }, []);
+
     return (
         <div className='Home'>
-            {data.length > 0 ? data.map(i => {
-                return <div onTransitionEnd={() => handleTransitionEnd()} className={`Cards ${like !== 0 ? like === 1 ? 'right' : 'left' : ''}`} key={i}>
-                    <UserCard setPlayingAudio={setPlayingAudio} playingAudio={playingAudio} interactCard={interactCard} i={i}></UserCard>
+            {data.length > 0 ? data.map((item, i) => {
+                return <div onTransitionEnd={() => handleTransitionEnd()} className={`Cards ${like !== 0 ? like === 1 ? 'right' : 'left' : ''}`} key={item.USER_ID}>
+                    <UserCard key={item.USER_ID} data={item} i={i+1} setPlayingAudio={setPlayingAudio} playingAudio={playingAudio} interactCard={interactCard} ></UserCard>
                 </div>
             }):  <span className='message'>No profiles have been found.</span>}
         </div>
--- frontend/src/components/UserCard/UserCard.jsx
+++ frontend/src/components/UserCard/UserCard.jsx
@@ -1,11 +1,7 @@
 import React, {useState, useEffect}  from 'react'
 import './UserCard.css'
-import axios from "axios";
-import { useParams, Link } from "react-router-dom";
 
-
-
-export default function UserCard({interactCard, i, playingAudio, setPlayingAudio}) {
+export default function UserCard({interactCard, data, i, playingAudio, setPlayingAudio}) {
     let play = false
 
     let audio = new Audio(require("../../audios/Peaches.mp3"))
@@ -23,25 +19,11 @@ export default function UserCard({interactCard, i, playingAudio, setPlayingAudio
         play = !play
         
     }
-    const routeParams = useParams();
-	const id = routeParams.id;
-    const [data, setData] = useState ([]);
 
-    const loadData = async () => {
-        const response = await axios.get("https://minireto-api.vercel.app/getUsers/"+i);
-        setData(response.data);
-    };
-
-    useEffect(() => {
-        loadData();
-    }, );
-
+    console.log(data)
 
     return (
-        <div>
-                {data.map((item, index) => {
-        return (
-            <div key={item.USER_ID} className='UserCard' style={{"backgroundImage":`url(${require('../../img/'+i+'.webp')})`}}>
+            <div  className='UserCard' style={{"backgroundImage":`url(${require('../../img/'+data.USER_ID+'.webp')})`}}>
             <div className='usercard-container'>
                 <div className='row-buttons'>
                     <button className='like-button' onClick={() => interactCard(2, i)}><img alt='dislike' src={require("../../img/dislike.png")}/></button>
@@ -51,21 +33,15 @@ export default function UserCard({interactCard, i, playingAudio, setPlayingAudio
                 <div className='card-info'>
                     <div className='cardInfo-container'>
                         <div className='card-header'>
-                        <h2 >{data[0].USER_NAME}</h2>
+                        <h2 >{data.USER_NAME}</h2>
                         <button className='play-button' onClick={() => start()}></button>
                     </div>
-                    <span>Looking For: {data[0].LOOKING_FOR}</span>
-                    <p>{data[0].MESSAGE}</p>
+                    <span>Looking For: {data.LOOKING_FOR}</span>
+                    <p>{data.MESSAGE}</p>
                     </div>
-                </div>
-                <div>
                 </div>
             </div>
         </div>
-        );
-    })}
-        </div>
-
     )
 
 
